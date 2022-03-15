6744966a49964f39a4d935aa1085ad72593e0013
udaypalecha <99619714+udaypalecha@users.noreply.github.com>
2022-03-15 17:15:49 +0530

    Update IST_Time.py

--- IST_Time.py
+++ IST_Time.py
@@ -3,8 +3,8 @@ import datetime; print("\n")
 issue = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30) #Adding 5:30 hrs to UCT Time
 issue = datetime.datetime.strftime(issue,"%d %B %Y")
 print(f"Date of issue:    {issue}\n")
-issue = datetime.datetime.strptime(issue,"%d %B %Y")       #Date of Issue
+issue = datetime.datetime.strptime(issue,"%d %B %Y")        #Date of Issue
 
-reissue = issue + datetime.timedelta(days=21)              #Book return date
-reissue = datetime.datetime.strftime(reissue,"%d %B %Y")
+reissue = issue + datetime.timedelta(days=21)               #Adding 21 days to issue date
+reissue = datetime.datetime.strftime(reissue,"%d %B %Y")    #Book return date
 print(f"Book return date: {reissue}")
