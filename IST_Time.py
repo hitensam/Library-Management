5eb0d1ba452748fb7d7a25158510d4ac18d3cd28
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2022-03-15 23:19:07 +0530

    added reissue button working
    reissue button working

--- IST_Time.py
+++ IST_Time.py
@@ -1,10 +1,26 @@
-import datetime; print("\n")
+import datetime; # print("\n")
 
-issue = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30) #Adding 5:30 hrs to UCT Time
-issue = datetime.datetime.strftime(issue,"%d %B %Y")
-print(f"Date of issue:    {issue}\n")
-issue = datetime.datetime.strptime(issue,"%d %B %Y")       #Date of Issue
+INDIA_TIME = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30) #Adding 5:30 hrs to UCT Time
 
-reissue = issue + datetime.timedelta(days=21)              #Book return date
-reissue = datetime.datetime.strftime(reissue,"%d %B %Y")
-print(f"Book return date: {reissue}")
+def ISSUE():
+    global INDIA_TIME
+    issue = datetime.datetime.strftime(INDIA_TIME,"%d %B %Y")  #Date of Issue
+    # issue = datetime.datetime.strptime(issue,"%d %B %Y")   
+    return (issue)
+# print(f"Date of issue:    {issue}\n")
+   
+
+
+def REISSUE(issue):
+    # global INDIA_TIME  #is not using global time as Uday Says
+    issue = datetime.datetime.strptime(issue,"%d %B %Y") #converts string to datetime format.
+    reissue = issue + datetime.timedelta(days=25)              #Here issue type should be datetime for it to work.
+    reissue = datetime.datetime.strftime(reissue,"%d %B %Y") #Book return date
+    return (reissue)
+
+# print(f"Book return date: {reissue}")
+
+# a0 = ISSUE()
+# print(a0)
+# a = REISSUE('22 March 2022')
+# print(a)
